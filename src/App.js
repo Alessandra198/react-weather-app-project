66ffdafe706ba4d1c4128c96c64d39d77fee7f0a
Alessandra198 <rinaldoalessandra@gmail.com>
2024-03-29 21:35:03 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/Alessandra198"
-          rel="noreferrer"
-          target="_blank"
-        >
-          Alessandra Rinaldo
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Alessandra198/react-weather-app-project"
-          rel="noreferrer"
-          target="_blank"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Alessandra198"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Alessandra Rinaldo
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Alessandra198/react-weather-app-project"
+            rel="noreferrer"
+            target="_blank"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
