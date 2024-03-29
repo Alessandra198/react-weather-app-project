e3ff5eed043807c8bbb669bc9a2fc55afd6c7ca5
Alessandra198 <rinaldoalessandra@gmail.com>
2024-03-29 21:24:46 +0100

    Added link repository and git profile

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/Alessandra198"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Alessandra Rinaldo
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Alessandra198/react-weather-app-project"
+          rel="noreferrer"
+          target="_blank"
+        >
+          open-sourced
+        </a>
+      </footer>
     </div>
   );
 }
