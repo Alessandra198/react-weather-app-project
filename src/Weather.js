f6f50cee7a6a221f66e31807def25c24f270f59f
Alessandra198 <rinaldoalessandra@gmail.com>
2024-04-02 17:05:14 +0200

    Added APi integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,62 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city.."
-              className="form-control"
-            />
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      city: response.data.city,
+      date: "Sunday, 07:00",
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      humidity: response.data.temperature.humidity,
+      description: response.data.condition.description,
+      icon: response.data.condition.icon_url,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-control w-100"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-control w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <main>
-        <h1>Paris</h1>
-        <ul>
-          <li>Wednesday 07:00</li>
-          <li>Mostly Cloudy</li>
-          <div className="row mt-3">
-            <div className="col-6">
-              <div className="clearfix d-flex">
-                <img
-                  src="https://ssl.gstatic.com/onebox/weather/64/windy.png"
-                  alt="meteo icon"
-                  className="float-left"
-                />
-                <div className="float-left">
-                  <span className="temperature">6</span>
-                  <span className="unit">°C</span>
+        </form>
+        <main>
+          <h1>{weatherData.city}</h1>
+          <ul>
+            <li>{weatherData.date}</li>
+            <li className="text-capitalize">{weatherData.description}</li>
+            <div className="row mt-3">
+              <div className="col-6">
+                <div className="clearfix d-flex">
+                  <img
+                    src={weatherData.icon}
+                    alt={weatherData.description}
+                    className="float-left"
+                  />
+                  <div className="float-left">
+                    <span className="temperature">
+                      {Math.round(weatherData.temperature)}
+                    </span>
+                    <span className="unit">°C</span>
+                  </div>
                 </div>
               </div>
+              <div className="col-6">
+                <ul>
+                  <li>
+                    Humidity: <strong>{weatherData.humidity}%</strong>
+                  </li>
+                  <li>
+                    Wind: <strong>{weatherData.wind} km/h</strong>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className="col-6">
-              <ul>
-                <li>
-                  Precipitation: <strong>15%</strong>
-                </li>
-                <li>
-                  Humidity: <strong>70%</strong>
-                </li>
-                <li>
-                  Wind: <strong>20 km/h</strong>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </ul>
-      </main>
-    </div>
-  );
+          </ul>
+        </main>
+      </div>
+    );
+  } else {
+    const apiKey = "cb4440cc8b49o2e10d5a63f3ecftcafe";
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading..";
+  }
 }
