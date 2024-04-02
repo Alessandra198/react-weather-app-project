143c0c00ed52eabb5f0097d84f51de82ef185ff5
Alessandra198 <rinaldoalessandra@gmail.com>
2024-04-02 22:01:51 +0200

    Added WeatherForecastDay component

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,30 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import WeatherIcon from "./WeatherIcon";
+import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
-    function handleResponse(response) {
-        console.log(response.data);
-    } 
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
 
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
 
+  if (loaded) {
+    console.log(forecast);
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]}/>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
     let apiKey = "cb4440cc8b49o2e10d5a63f3ecftcafe";
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.city}&key=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="day">Thu</div>
-          <WeatherIcon code="clear-sky-day" size={36}/>
-          <div className="temperatures">
-            <span className="temperature-max">19°</span>
-            <span className="temperature-min">10°</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+
+    return null;
+  }
 }
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,31 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+
+export default function WeatherForecastDay(props) {
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temperature.maximum);
+    return `${temperature}°`;
+  }
+  function minTemperature() {
+    let temperature = Math.round(props.data.temperature.minimum);
+    return `${temperature}°`;
+  }
+
+  return (
+    <div className="WeatherForecastDay">
+      <div className="day">{day()}</div>
+      <WeatherIcon code={props.data.condition.icon} size={36} />
+      <div className="temperatures">
+        <span className="temperature-max">{maxTemperature()}</span>
+        <span className="temperature-min">{minTemperature()}</span>
+      </div>
+    </div>
+  );
+}
